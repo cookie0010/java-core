161d7968956b321871175a878357410e19482aa0
cookie0010 <sg.been2017@gmail.com>
2023-01-05 01:03:02 +0900

    @Configuration

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -16,4 +16,9 @@ public class MemberServiceImpl implements MemberService{
     public Member findmember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    // 테스트용
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
